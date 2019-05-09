99c35bc7101067727f915846c244909966411cbd
Chaitanya Narukulla <chaitanyanarukulla@gmail.com>
2019-05-09 14:30:09 -0700

    Update map.js

--- src/components/map.js
+++ src/components/map.js
@@ -12,7 +12,7 @@ class Map extends React.Component{
   }
   componentDidUpdate=()=>{
     let newMapUrl =`https://maps.googleapis.com/maps/api/staticmap?center=${this.props.data.body.latitude}%2c%20${this.props.data.body.longitude}&zoom=13&size=600x300&maptype=roadmap
-    &key=AIzaSyDHn_Xd-DNpqT7JDAbLqJ9OwuvU5Una2Iw`;
+    &key=GEO_API_Key`;
     console.log(this.props.location)
     if(this.state.mapUrl !== newMapUrl){
       this.setState({mapUrl:newMapUrl})
